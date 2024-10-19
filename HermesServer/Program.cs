2967916523be799e2a4edf36260aa0fb95ee85b1
Shirokov Ivan <literallyshiva@gmail.com>
2024-10-19 14:22:23 +0300

    Added ChatHub, JSClient and index.html.

--- HermesServer/Program.cs
+++ HermesServer/Program.cs
@@ -1,6 +1,14 @@
+using HermesServer.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
---
+++ HermesServer/wwwroot/js/hermesJSClient.js
@@ -0,0 +1,26 @@
+﻿const hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")
+    .build();
+
+document.getElementById("sendButton").addEventListener("click", function () {
+    let message = document.getElementById("message").value;
+    hubConnection.invoke("SendMessage", message)
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+});
+
+hubConnection.on("Receive", function (message) {
+
+    let messageElement = document.createElement("p");
+    messageElement.textContent = message;
+    document.getElementById("chatroom").appendChild(messageElement);
+});
+
+hubConnection.start()
+    .then(function () {
+        document.getElementById("sendButton").disabled = false;
+    })
+    .catch(function (err) {
+        return console.error(err.toString());
+    });
