113fca545b8ee7bc06a951d9c42f7ab5777b15da
Shirokov Ivan <literallyshiva@gmail.com>
2024-10-21 14:01:17 +0300

    Minor naming fixes and appsettings fix.

--- HermesServer/wwwroot/js/login.js
+++ HermesServer/wwwroot/js/login.js
@@ -1,5 +1,5 @@
 ﻿document.getElementById("loginButton").addEventListener("click", function () {
     const userName = document.getElementById("userName").value;
     localStorage.setItem('userName', userName);
-    window.location.href = '/html/hermesJSClient.html'
+    window.location.href = '/html/chat.html'
 });
